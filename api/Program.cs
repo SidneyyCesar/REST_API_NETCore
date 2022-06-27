7188d3ca195a9d772824d53cd5d63398bd8462fa
Sidney Cesar <sidneyycesar@hotmail.com>
2022-06-27 17:37:52 -0300

    add api project

---
+++ api/Program.cs
@@ -0,0 +1,27 @@
+using application.Services.Users;
+using IoC.DI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Startup.ConfigureDependenceInjection(builder.Services);
+ConfigurationIOC.Load(builder.Services);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
