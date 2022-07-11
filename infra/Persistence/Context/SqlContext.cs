4efdb76e82ca79ef555c6fe304ce55af6c052cf1
Sidney Cesar <sidneyycesar@hotmail.com>
2022-07-11 15:32:30 -0300

    configure generics entity repository

--- infra/Persistence/Context/SqlContext.cs
+++ infra/Persistence/Context/SqlContext.cs
@@ -9,16 +9,19 @@ namespace infra.Data
 
         public SqlContext(DbContextOptions<SqlContext> options): base(options) { }
      
-        public DbSet<Users> Usuario { get; set; }
+        public DbSet<Users> users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             var userModel = modelBuilder.Entity<Users>();
            
-            userModel.Property(p => p.email).HasMaxLength(200);
-            userModel.Property(p => p.name).HasMaxLength(100);
-            userModel.Property(p => p.password).HasMaxLength(50);
-            userModel.Property(p => p.status).HasMaxLength(1);
+            userModel.Property(p => p.id).HasColumnName("Id");
+            userModel.Property(p => p.email).HasMaxLength(200).HasColumnName("Email");
+            userModel.Property(p => p.name).HasMaxLength(100).HasColumnName("Name");
+            userModel.Property(p => p.password).HasMaxLength(50).HasColumnName("Password");
+            
+
+            userModel.ToTable("Usuarios");
 
             base.OnModelCreating(modelBuilder);
         }
--- infra/Persistence/Repository/Persistence/RepositoryBase.cs
+++ infra/Persistence/Repository/Persistence/RepositoryBase.cs
@@ -2,74 +2,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using domain.Core.Interfaces.Repositories;
+using domain.Interfaces.Repositories;
 using infra.Data;
 using Microsoft.EntityFrameworkCore;
 
 namespace infra.Repository.Persistence
 {
-  public abstract class RepositoryBase<T>: IRepositoryBase<T> where T: class
-    {
-        private readonly SqlContext sqlContext;
+  public abstract class RepositoryBase<T>: IRepositoryBase<T> where T: class, IEntity
+  {
+    private readonly SqlContext sqlContext;
 
-        public RepositoryBase(SqlContext sqlContext)
-        {
-            this.sqlContext = sqlContext;
-        }
+    public RepositoryBase(SqlContext sqlContext)
+    {
+        this.sqlContext = sqlContext;
+    }
 
     public virtual async Task Add(T entity)
     {
-      try
-      {
-        sqlContext.Set<T>().Add(entity);
-        await sqlContext.SaveChangesAsync();
-      }
-      catch (System.Exception)
-      {
-        throw;
-      }
+      sqlContext.Set<T>().Add(entity);
+      await sqlContext.SaveChangesAsync();
     }
 
-    public virtual IEnumerable<T> GetAll()
+    public virtual async Task<IEnumerable<T>> GetAll()
     {
-      try
-      {
-        return sqlContext.Set<T>().ToList();
-      }
-      catch (System.Exception)
-      {        
-        throw;
-      }
+      return await sqlContext.Set<T>().ToListAsync();
     }
 
-    public virtual Task<T> GetById(int id)
+    public virtual async Task<T> GetById(int id)
     {
-      throw new System.NotImplementedException();
+      return await sqlContext.Set<T>().Where(x=> x.id == id).FirstAsync();      
     }
-
-    public virtual async Task Remove(T entity)
+    public virtual async Task Save(T entity)
     {
-      try
-      {
-        sqlContext.Set<T>().Remove(entity);
-        await sqlContext.SaveChangesAsync();
-      }
-      catch (System.Exception)
-      {        
-        throw;
-      }
+      sqlContext.Set<T>().Remove(entity);
+      await sqlContext.SaveChangesAsync();
     }
-
     public virtual async Task Update(T entity)
     {
-      try
-      {
-        sqlContext.Entry(entity).State = EntityState.Modified;
-        await sqlContext.SaveChangesAsync();
-      }
-      catch (System.Exception)
-      {        
-        throw;
-      }
+      sqlContext.Entry(entity).State = EntityState.Modified;
+      await sqlContext.SaveChangesAsync();
     }
+    public virtual async Task Remove(T entity)
+    {
+      sqlContext.Set<T>().Remove(entity);
+      await sqlContext.SaveChangesAsync();
+    }
+
   }
 }
